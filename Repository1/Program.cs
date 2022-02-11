b15d0a0d673f3383716fe555372692780b1c50e4
SerraCanca <serracanca@gmail.com>
2022-02-10 21:41:00 -0500

    update

---
+++ Repository1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyConsoleApplication
+{
+    class Student
+    {
+        public string name;
+        public int id;
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Serra = new Student();
+            Serra.name = "Serra";
+            Serra.id = 1;
+
+            var Chase = new Student();
+            Chase.name = "Chase";
+            Chase.id = 2;
+        }
+    }
+
+}
